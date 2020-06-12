fa7b0d46010e6b8927b159c37d3e09edf3180f3f
Andres Gallego <andres.gallego@sellics.com>
2020-06-12 13:15:40 +0200

    feat: add authentication

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,7 +5,8 @@ import { IdentityContextProvider } from "react-netlify-identity-widget";
 import "./layout.css";
 
 const Layout = ({ children }) => (
-  <IdentityContextProvider url="https://jamstack-intro-auth.netlify.com">
+  // <IdentityContextProvider url="https://jamstack-intro-auth.netlify.com">
+  <IdentityContextProvider url="https://jamstack-intro-auth-by-andresgallego.netlify.app">
     <header>
       <Link to="/">JAMstack App</Link>
     </header>
